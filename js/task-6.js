d78a34a18b85e45a30df64ae6001ad9e1afb5987
Alex Khavin <alexandr_havin@yahoo.com>
2025-02-16 22:10:43 +0200

    ready hw-07

---
+++ js/task-6.js
@@ -0,0 +1,34 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+// const controls = document.querySelector("#controls");
+
+// const inputNumber = document.querySelector(".input");
+// inputNumber.addEventListener("input", (event) => {
+//   const input = event.target;
+//   const result = input.value;
+//   console.log(result)
+// });
+
+// const createBtn = document.querySelector(".create-btn");
+// createBtn.addEventListener("click", event => {
+  
+
+// })
+
+// const destroyBtn = document.querySelector(".destroy-btn");
+// // destroyBtn.addEventListener("click",)
+
+// const boxes = document.querySelector("#boxes");
+// boxes.style.width = "30px";
+// boxes.style.height = "30px";
+// boxes.style.backgroundColor = getRandomHexColor();
+
+
+// // ЩЕ ДУМАЮ НАД ЦІЄЮ ЗАДАЧОЮ
+
+
+
