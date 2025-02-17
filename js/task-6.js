1784956ad8ed5ada2305407350e73a366df985de
Alex Khavin <alexandr_havin@yahoo.com>
2025-02-17 20:55:17 +0200

    add task 6

--- js/task-6.js
+++ js/task-6.js
@@ -7,27 +7,34 @@ function getRandomHexColor() {
 const controls = document.querySelector("#controls");
 
 const inputNumber = document.querySelector(".input");
-inputNumber.addEventListener("input", getNumber);
-
-function getNumber(event) {
-  const input = event.target;
-  const result = input.value;
-}
 
 const boxes = document.querySelector("#boxes");
 
 const createBtn = document.querySelector(".create-btn");
 createBtn.addEventListener("click", () => {
-  const countDiv = parseInt(inputNumber.value);
+const countDiv = parseInt(inputNumber.value);
 
-  const markupBox = document.createElement("div");
-  markupBox.style.width = "30px";
-  markupBox.style.height = "30px";
-  markupBox.style.backgroundColor = getRandomHexColor();
-  boxes.append(markupBox);
-})
+if (countDiv > 0 && countDiv <= 100) {
+  createBoxes(countDiv);
+  inputNumber.value = "";
+}
+  });
+
+function createBoxes(amount) {
+  let markup = "";
+  let size = 30;
+ 
+  for (let i = 0; i < amount; i++) {
+    markup += `<div style="width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()};"></div>`;
+    size += 10;
+  }
+  boxes.innerHTML = markup;
+  
+};
 
 const destroyBtn = document.querySelector(".destroy-btn");
-// destroyBtn.addEventListener("click",)
+destroyBtn.addEventListener("click", destroyBoxes)
 
-// ЩЕ ДУМАЮ НАД ЦІЄЮ ЗАДАЧОЮ
+function destroyBoxes() {
+  boxes.innerHTML = "";
+};
